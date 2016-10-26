1976e1ec344c766770784f19a71ad9ba09faf9cf
zhengxiaoyao0716 <1499383852@qq.com>
2016-10-26 22:52:47 +0800

    完善启动器，添加ReadME
    Signed-off-by: zhengxiaoyao0716 <1499383852@qq.com>

--- launcher.bat
+++ launcher.bat
@@ -1,2 +1,18 @@
 @echo off
-python35_with_matplotlib\python.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+set name=%1
+if not "%name%"=="" goto LAUNCH
+
+:CHOOSE
+set /p num=请选择题号（1、2、3、4）：
+for %%i in (1 2 3 4) do (
+    if %num% EQU %%i (
+        set name=question%num%.py
+        goto LAUNCH
+    )
+)
+echo 无效的输入，请重新选择
+goto CHOOSE
+
+:LAUNCH
+python35_with_matplotlib\python.exe %name% %2 %3 %4 %5 %6 %7 %8 %9
